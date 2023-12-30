dcd1faa4c3eaf332855dd0f4f4abca8a0a44941d
ashmin78 <155237232+ashmin78@users.noreply.github.com>
2023-12-31 01:08:54 +0545

    Update script.sh
    log in git checkout

--- script.sh
+++ script.sh
@@ -23,12 +23,12 @@ git clone --bare https://github.com/ashmin78/dotfiles.git $HOME/dotfiles
 alias dotfile="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 dotfile checkout
 if [ $? = 0 ]; then
-	echo "clone sucessful !!";
+	log "clone sucessful !!";
 else
-	echo "Existing files found. Backing up pre-existing dot files.";
+	log "Existing files found. Backing up pre-existing dot files.";
 	mkdir -p $HOME/.dotfiles_bkp
 	dotfile checkout 2>$1 | egrep "\s+\." | awk {'print $1'} | xarg -I{} mv {} $HOME/.dotfiles_bkp/{}
-	echo "clone sucessful !!";
+	log "clone sucessful !!";
 fi;
 dotfile checkout
 log "=================================================="
@@ -42,7 +42,7 @@ log "=================================================="
 log "=================================================="
 log "installing brave browser ..."
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+ "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
 sudo apt install brave-browser -y
 log "brave browser installation completed !!!"
@@ -53,7 +53,7 @@ log "installing vscode ..."
 sudo apt-get install wget gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo sh -c ' "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https -y
 sudo apt update -y
@@ -73,5 +73,5 @@ log "=================================================="
 log "=================================================="
 log "running post installation work ..."
 # ~/miniconda3/bin/conda init bash
-# echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
+#  'eval "$(starship init bash)"' >> $HOME/.bashrc
 echo 'alias dotfile="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"' >> $HOME/.bashrc
