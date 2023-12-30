c11e52949fecfda28d8dc775ca7b372656e849b0
ashmin78 <155237232+ashmin78@users.noreply.github.com>
2023-12-31 02:15:17 +0545

    Update script.sh
    bug fix: remove alias

--- script.sh
+++ script.sh
@@ -19,18 +19,18 @@ log "=================================================="
 log "=================================================="
 log "cloning dotfiles"
 git clone --bare https://github.com/ashmin78/dotfiles.git $HOME/dotfiles
-alias dotfile="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
-dotfile checkout
+dotfile="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+$dotfile checkout
 if [ $? = 0 ]; then
 	log "clone sucessful !!";
 else
 	log "Existing files found. Backing up pre-existing dot files.";
 	# mkdir -p $HOME/.dotfiles_bkp
-	dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} dirname {} | uniq | xargs -I{} mkdir -p $HOME/.dotfiles_bkp/{}
-	dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles_bkp/{}
+	$dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} dirname {} | uniq | xargs -I{} mkdir -p $HOME/.dotfiles_bkp/{}
+	$dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles_bkp/{}
 	log "clone sucessful !!";
 fi;
-dotfile checkout
+$dotfile checkout
 log "=================================================="
 
 log "=================================================="
